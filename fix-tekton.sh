8861e30eae05763e35f1d90418138c55c1d4710e
Alex Szczuczko <aszczucz@redhat.com>
2024-03-13 12:25:49 -0600

    Add script that modifies the tekton files so they work with an FBC

---
+++ fix-tekton.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -euo pipefail
+
+join_by() {
+    local IFS="$1"
+    shift
+    echo "$*"
+}
+
+remove=(
+    clair-scan
+    clamav-scan
+    prefetch-dependencies
+    sast-snyk-check
+    ecosystem-cert-preflight-checks
+)
+
+# Delete the given tasks that shouldn't be there
+task_pattern="$(printf '.name == "%s" or ' "${remove[@]}" | sed 's/ or $//')"
+find .tekton/ -type f -name '*.yaml' | xargs -n1 yq -i e "del(.spec.pipelineSpec.tasks | .[] | select($task_pattern))"
+
+# Delete any dependency on the removed tasks
+dep_pattern="$(printf '. == "%s" or ' "${remove[@]}" | sed 's/ or $//')"
+find .tekton/ -type f -name '*.yaml' | xargs -n1 yq -i e "del(.spec.pipelineSpec.tasks | .[] | .runAfter | .[] | select($dep_pattern))"
